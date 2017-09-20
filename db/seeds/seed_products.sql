c59f1915c80cd6db08c1ed763cb448704b60d68f
Rastko Sasic <rsasic@europe.com>
2017-09-20 18:45:41 -0400

    database rotaflow created. table users creted.

---
+++ db/seeds/seed_products.sql
@@ -0,0 +1,5 @@
+\c rotaflow_dev
+
+INSERT INTO products (title, imgURL, productURL) VALUES
+  ('New Nike Flex 2016 RN Mens Running Training Shoes Photo Blue 830369 400','http://thumbs4.ebaystatic.com/pict/2630069861234040_1.jpg', 'http://www.ebay.com/itm/New-Nike-Flex-2016-RN-Mens…to-Blue-830369-400-/263006986123?var=562015275517'),
+  ('Allen Sports Urban 1-Speed Folding Bike - Red', 'http://thumbs1.ebaystatic.com/m/mlJ1zCZyktWc68v1xodQ-KA/140.jpg', 'http://www.ebay.com/itm/Allen-Sports-Urban-1-Speed-Folding-Bike-Red-/332374160132');
