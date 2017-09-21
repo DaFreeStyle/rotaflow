86a2a2654b7f5eb3ffa4e23b3eb715a0c5491930
Felipe Hernandez <felipeneo@hotmail.com>
2017-09-21 16:04:53 -0400

    create component class EbaySearchFrom, no form yet, just title

--- client/src/components/EbaySearchForm.js
+++ client/src/components/EbaySearchForm.js
@@ -1,1 +1,14 @@
-EbaySearchForm.js
+import React, { Component } from 'react';
+
+class EbaySearchForm extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Ebay Search Form</h1>
+      </div>
+
+    );
+  };
+};
+
+export default EbaySearchForm;
