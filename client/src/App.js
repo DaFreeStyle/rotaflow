599e52299edc1152bfb13af1f3e57961915c40c7
Felipe Hernandez <felipeneo@hotmail.com>
2017-09-21 16:27:30 -0400

    create props for EbaySearchForm in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       inputSearchValue: '',
     }
     this.handleInputSearchOnChange = this.handleInputSearchOnChange.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -35,7 +36,15 @@ class App extends Component {
   handleSearchSubmit(event) {
     event.preventDefault();
     let keywords = encodeURI(this.state.inputSearchValue);
-    let url = 'http://svcs.ebay.com/services/search/FindingService/v1?SERVICE-NAME=FindingService&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=FelipeHe-RotaFlow-PRD-25d7504c4-6d3d6a4d&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&GLOBAL-ID=EBAY-US&keywords=' + keywords + '&paginationInput.entriesPerPage=25&paginationInput.entriesPerPage=1'
+    let url = 'http://svcs.ebay.com/services/search/FindingService/v1?SERVICE-NAME=FindingService&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=FelipeHe-RotaFlow-PRD-25d7504c4-6d3d6a4d&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&GLOBAL-ID=EBAY-US&keywords=' + keywords + '&paginationInput.entriesPerPage=25&paginationInput.entriesPerPage=1';
+    axios(url)
+      .then((res) => {
+        this.setState({
+          products: res,
+        }).catch((err) => {
+          console.log(err);
+        })
+      })
   }
 
   render() {
@@ -48,7 +57,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <EbaySearchForm />
+        <EbaySearchForm
+          submitHandler={this.handleSearchSubmit}
+          handleInputSearchOnChange={this.handleInputSearchOnChange}
+          inputSearchValue={this.state.inputSearchValue}
+        />
       </div>
     );
   }
