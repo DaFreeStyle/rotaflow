b327b397e6e66fafe088fb1d6bf489a8a9b272c5
Felipe Hernandez <felipeneo@hotmail.com>
2017-09-21 17:58:26 -0400

    edit handleSearchSubmit axios then-catch method to prevent similar bug in previous componenDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -39,13 +39,14 @@ class App extends Component {
     let keywords = encodeURI(this.state.inputSearchValue);
     let url = 'http://svcs.ebay.com/services/search/FindingService/v1?SERVICE-NAME=FindingService&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=FelipeHe-RotaFlow-PRD-25d7504c4-6d3d6a4d&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&GLOBAL-ID=EBAY-US&keywords=' + keywords + '&paginationInput.entriesPerPage=25&paginationInput.entriesPerPage=1';
     axios(url)
-      .then((res) => {
+    .then((res) => {
+      console.log(res);
         this.setState({
           products: res,
-        }).catch((err) => {
-          console.log(err);
         })
-      })
+      }).catch((err) => {
+          console.log(err);
+      });
   }
 
   render() {
