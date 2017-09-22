67b22d891eaed8b6b144dd603a87f3b762a8940d
Felipe Hernandez <felipeneo@hotmail.com>
2017-09-21 23:56:14 -0400

    import EbaySearchForm component to DispalyEbayProductComponent and add h2

--- client/src/components/DisplayEbayProduct.js
+++ client/src/components/DisplayEbayProduct.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import EbaySearchForm from './EbaySearchForm';
 
 class DisplayEbayProduct extends Component {
   render() {
     return(
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <img src={this.props.product.galleryURL} />
-            </td>
-            <td>
-              <a href={this.props.product.viewItemURL} target='_blank'>{this.props.product.title}</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <h2>Dispaly Ebay Products</h2>
+        <EbaySearchForm />
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <img src={this.props.product.galleryURL} />
+              </td>
+              <td>
+                <a href={this.props.product.viewItemURL} target='_blank'>{this.props.product.title}</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
